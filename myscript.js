a4f5aedf6acf3e60c58d6d4599d825c5333b433b
sarahshy <sarahshy@Sarahs-MacBook-Air.local>
2016-10-08 17:33:55 -0400

    10/8-1

--- myscript.js
+++ myscript.js
@@ -1,16 +1,10 @@
-// $(document).ready(function() {
-//     checkbox()
-//     deleteButton()
-// });
-
 function addNewTask() {
     console.log("Add new task clicked");
     var text = $("#newTaskText").val();
     if (text == "")
         {
-
+            return false;
         } else {
-    //$("#taskList").append($("<li>").text(text));
     $("#taskList").prepend($("<li><input type = 'checkbox'> "+text+" </li>").append($("<button onclick = removeTask(this) class = delete>x</button>")));
     clearText();
     }
@@ -22,39 +16,6 @@ function clearText() {
 };
 
 
-
-
-
-
-
-
-
-///Checkboxes
-// function checkbox() {
-//     $('<input type="checkbox" value="1" class = "boxcheck"/>').prependTo('li');
-// }
-
-// function checkboxNewTask() {
-//     ($('<input type = "checkbox" class = "boxcheck">')).prependTo($("#taskList").children().last());
-// };
-
-// $('.boxcheck').click(function() {
-//     $().wrap("<strike>");
-// });
-
-// function deleteButton() {
-//     $('#taskList').children().append('<button class = "remove">x</button>');
-// }
-
- function removeTask(task) {
+function removeTask(task) {
     $(task).parent().remove();
- };
-
-
-// function deleteButton() {
-//     for (i = 0; i < $('li').length; i++) {
-//         console.log($('li:eq(i)')) //.append($('<button>'));
-//         console.log("test");
-//         //$('#tasklist').children().append('<button>x</button>');
-//     };
-// };
+};
